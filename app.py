3cc4323a5149feec04a1eb5b1a487bfe54a08618
Alca <dyd0927@gmail.com>
2025-02-18 17:23:45 +0900

    김용현_라우터 설정, 데이터 베이스 정의 설정

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
 # 모든 라우터 등의 정보가 들어갑니다.
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql+pymysql://root:1234@localhost:3306/EventSchedule'
+db = SQLAlchemy(app)
+
+# 데이터 모델 정의 (행사 정보)
+class Event(db.Model):
+    no = db.Column(db.Integer, primary_key=True)
+    eventName = db.Column(db.String(30), nullable=False)
+    startDate = db.Column(db.String(30), nullable=False)
+    endDate = db.Column(db.String(30), nullable=False)
+    location = db.Column(db.String(30), nullable=False)
+    explain = db.Column(db.String(500), nullable=False)
+    image = db.Column(db.String(30), nullable=False)
+
+    def __repr__(self):
+        return f'Event {self.eventName}'
+
+# 라우트
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+with app.app_context():
+    db.create_all()
